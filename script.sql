3a415b0c3d0e758f90d99ceca260ce55149f7b3f
Mateus Guimarães Alves <guimaraesalves45@gmail.com>
2021-02-20 21:44:14 -0300

    commit update comercial.sql

--- script.sql
+++ script.sql
@@ -89,6 +89,67 @@ primary key(n_numeivenda));
 
 desc comivenda;
 
+-- CUIDANDO DA INTEGRIDADE DO BANCO DE DADOS (FOREIGN KEY)
+
+alter table comvenda add constraint fk_comprodu_comforne
+foreign key(n_numeforne)
+references comforne(n_numeforne)
+on delete no action
+on update no action;
+
+alter table comvenda add constraint fk_comprodu_comvende
+foreign key(n_numevende)
+references comvende(n_numevende)
+on delete no action
+on update no action;
+
+alter table comvenda add constraint fk_comvenda_comclien
+foreign key(n_numeclien)
+references comclien(n_numeclien)
+on delete no action
+on update no action;
+
+alter table comivenda
+change column n_numeprod n_numeprodu int not null;
+
+alter table comivenda add constraint fk_comivenda_comprodu
+foreign key(n_numeprodu)
+references comprodu (n_numeprodu)
+on delete no action
+on update no action;
+
+alter table comivenda add constraint fk_comivenda_comvenda
+foreign key(n_numevenda)
+references comvenda (n_numevenda)
+on delete no action
+on update no action;
+
+
+-- ALTERANDO AS TABELAS.
+ alter table comclien add column c_cidaclien varchar(50);
+ 
+ alter table comclien add column c_estaclien varchar(50);
+ 
+ alter table comclien modify column c_estaclien varchar(100);
+ alter table comclien modify column c_estaclien varchar(30);
+ desc comclien;
+ 
+ desc comvendas;
+ 
+ 
+ -- EXCLUINDO (DROPANDO) AS TABELAS.
+ 
+ drop table comvendas;
+ show tables;
+ 
+
+
+
+
+
+
+
+
 
 
 
